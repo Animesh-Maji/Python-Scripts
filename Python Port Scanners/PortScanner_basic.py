386aa2e8cbe5146495d1f21ff75a8c25ec9ee91d
Animesh <animeshmaji139@gmail.com>
2022-08-04 13:02:56 +0530

    fixed user input and added some useful comments

--- Python Port Scanners/PortScanner_basic.py
+++ Python Port Scanners/PortScanner_basic.py
@@ -4,14 +4,21 @@ from IPy import IP
 def port_scan(ipaddr, port):
     try :
         sock = socket.socket()
-        sock.settimeout(0.5) # this will try to connect to a port for defined time either this take long time to connect
+        sock.settimeout(0.5) # this will try to connect to a port for the defined time either it will take long time to connect
         sock.connect((ipaddr,port))
+
         print("[+] The Port " + str(port) + " is open")
+
     except : 
         pass
-        # print("[-] The port " + str(port) + " is closed")
 
-ipaddr = input("Enter the target Ip : ")
+        # print("[-] The port " + str(port) + " is closed")  I commented out this because we dont want to see all the closed ports
 
-for port in range(130,140):
+ipaddr = input("Enter the target Ip : ") 
+portRange = input("Enter the port range (eg. 80-445) : ")
+
+lowPort = int(portRange.split("-")[0])
+highPort = int(portRange.split("-")[1])
+
+for port in range(lowPort, highPort): 
     port_scan(ipaddr, port)
